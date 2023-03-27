7d2543534b640013c1dc291752aeca78e3ce59c6
JohnnyJohnJohns <johnhealy123@gmail.com>
2023-03-27 14:43:42 +0100

    Added Wander function to AI

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+
+    public float timer, wanderTime;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +28,27 @@ public class AIController : MonoBehaviour
                 SetAITargetLocation(hit.point);
             }
         }
+
+        timer += Time.deltaTime;
+        
+        Wander();
     }
 
     private void SetAITargetLocation(Vector3 targetLocation)
     {
         _navMeshAgent.SetDestination(targetLocation);
+    }
+
+
+    private void Wander()
+    {
+        if (timer >= wanderTime)
+        {
+            Vector2 wanderTarget = Random.insideUnitCircle * wanderTime;
+            Vector3 wanderPos3D = new Vector3(transform.position.x + wanderTarget.x, transform.position.y,
+                transform.position.z + wanderTarget.y);
+            SetAITargetLocation(wanderPos3D);
+            timer = 0;
+        }
     }
 }
